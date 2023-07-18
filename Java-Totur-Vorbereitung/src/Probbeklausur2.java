f5c6697ee6972d5279c4b5b03c1b9bfd2ae9bbcc
Ajouka <tarek.ajouka@outlook.com>
2023-07-18 10:25:19 +0200

    done

--- Java-Totur-Vorbereitung/src/Probbeklausur2.java
+++ Java-Totur-Vorbereitung/src/Probbeklausur2.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Probbeklausur2 {
     public static boolean pruefeMatrix(int[][] matrix) {
         int test = 0;
@@ -28,11 +33,46 @@ public class Probbeklausur2 {
         return s.replaceAll(" +", " ");
     }
 
+    public static double[] liesDaten(String filename) throws FileNotFoundException {
+        File f = new File(filename);
+        Scanner c = new Scanner(f);
+        String line = "";
+        do {
+            line = c.nextLine();
+        }
+        while (line.charAt(0) == '#');
+        String[] splt = line.split(":");
+        int count = Integer.parseInt(splt[1].trim());
+        double[] res = new double[count];
+        for (int i = 0; i < count; i++) {
+            do {
+                line = c.nextLine();
+            }
+            while (line.charAt(0) == '#');
+            res[i] = Double.parseDouble(line);
+        }
+        c.close();
+        return res;
+    }
+
+
+    public static boolean stimmenUeberein(String s1, String s2) {
+        int twinCount = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) continue;
+            else if (i < s1.length() - 1 && s1.charAt(i) == s2.charAt(i + 1) && s1.charAt(i + 1) == s2.charAt(i) && twinCount == 0) {
+                twinCount++;
+                i++;
+            } else return false;
+        }
+
+        return true;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(verschoenere("Tarek Ajouka     13years old"));
-        int[][] x = new int[][]{{1, 2, 3}, {2, 3, 1}, {3, 1, 2}};
-        System.out.println(pruefeMatrix(x));
+        System.out.println(stimmenUeberein("funktion", "funktiom"));
 
 
     }
