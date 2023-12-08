aabb2acaddffd53500e745b701638af7dcbf233d
Ajouka <bx244480@win.rz.rwth-aachen.de>
2023-12-08 08:08:02 +0100

    refs main day2.1.1

--- AdventCode23/src/day2.java
+++ AdventCode23/src/day2.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -7,25 +9,28 @@ import java.util.List;
 public class day2 {
 
     public static int counter(String filePath) throws IOException {
+        File f = new File("AdventCode23/src/Out.dat");
+        PrintWriter pw = new PrintWriter(f);
         int sum = 0;
         List<String> lines = Files.readAllLines(Path.of(filePath));
         for (String x : lines) {
-            int num = Integer.parseInt("" + x.charAt(5));
+            String num = x.split(" ")[1];
+            int num1 = Integer.parseInt(num.substring(0, num.length() - 1));
             boolean poosible = true;
 
             String[] bags = x.split(";");
             for (String x1 : bags) {
-                if (Integer.parseInt("" + x1.charAt(x1.indexOf("red") - 2)) > 12) {
+                if (x1.contains("red") && x1.charAt(x1.indexOf("red") - 3) != ' ' && Integer.parseInt("" + x1.charAt(x1.indexOf("red") - 2) + x1.charAt(x1.indexOf("red") - 3)) > 12) {
                     poosible = false;
                     break;
 
                 }
-                if (Integer.parseInt("" + x1.charAt(x1.indexOf("green") - 2)) > 13) {
+                if (x1.contains("green") && x1.charAt(x1.indexOf("green") - 3) != ' ' && Integer.parseInt("" + x1.charAt(x1.indexOf("green") - 2) + x1.charAt(x1.indexOf("green") - 3)) > 13) {
                     poosible = false;
                     break;
 
                 }
-                if (Integer.parseInt("" + x1.charAt(x1.indexOf("blue") - 2)) > 14) {
+                if (x1.contains("blue") && x1.charAt(x1.indexOf("blue") - 3) != ' ' && Integer.parseInt("" + x1.charAt(x1.indexOf("blue") - 2) + x1.charAt(x1.indexOf("blue") - 3)) > 14) {
                     poosible = false;
                     break;
 
@@ -33,19 +38,19 @@ public class day2 {
 
 
             }
-            if (!poosible) sum += num;
-
-
+            if (!poosible) {
+                sum += num1;
+                pw.println("**" + x);
+            } else pw.println(x);
         }
+        pw.close();
         return sum;
     }
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println(counter("/Users/tarkoowolf/Workspace/AdventCode23" +
-                "/src/input2.dat"));
-
-
+        System.out.println(counter("C:\\Workspace\\Workspace\\AdventCode23\\src\\input.dat"));
 
     }
 }
+
