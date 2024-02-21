13b4f99c2b9e8c2b214f686aa4dc59419e6ca07d
TarekAjouka <128493563+TarekAjouka@users.noreply.github.com>
2024-02-22 00:04:49 +0100

    refs main neue klausur

--- Algo/Algo01/src/NodeList1.java
+++ Algo/Algo01/src/NodeList1.java
@@ -1,3 +1,5 @@
+
+
 public class NodeList1 <Object>{
 
 
---
+++ KlausurVorbereitung/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<Integer> getValues(int num) {
+        return getValues(num, 2);
+    }
+
+    private static ArrayList<Integer> getValues(int num, int fac) {
+        if (num == 1) {
+            return new ArrayList<Integer>();
+        } else if (num % fac == 0) {
+            ArrayList<Integer> ret = getValues(num / fac, fac);
+            ret.add(fac);
+            return ret;
+        } else {
+            return getValues(num, fac + 1);
+        }
+    }
+
+    public static int sucheAB(String[] text){
+        int counter=0;
+
+        for(int j=0;j<text.length;j++){
+            String s=text[j];
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='a'&&i<s.length()-1&&s.charAt(i+1)=='b')counter++;
+                if(s.charAt(i)=='a'&& i==s.length()-1 && j<text.length-1){
+                    counter++;
+                }
+            }
+
+        }
+
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+
+        String[] text={"abcccdddda","bcccccaba","bab"};
+        System.out.println(sucheAB(text));;
+
+    }
+}
