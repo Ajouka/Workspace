934c8cf53ccdee73f4d09432d45f3ee9ad723b9c
TarekAjouka <128493563+TarekAjouka@users.noreply.github.com>
2024-05-22 11:56:24 +0200

    refs main Sudoku fertig

--- Java/Klausur2024/src/Main.java
+++ Java/Klausur2024/src/Main.java
@@ -1,32 +1,27 @@
 public class Main {
 
-    public static void show(int[][] x, int row, int col) {
-        int startRow = row - row % 3;
-        int startCol = col - col % 3;
-
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                System.out.println(x[i][j]);
-            }
-        }
-
-    }
 
     public static void main(String[] args) {
+
         int[][] board = new int[][]{
-                {5, 3, 0, 0, 7, 0, 0, 0, 0},
-                {6, 0, 0, 1, 9, 5, 0, 0, 0},
-                {0, 9, 8, 0, 0, 0, 0, 6, 0},
-                {8, 0, 0, 0, 6, 0, 0, 0, 3},
-                {4, 0, 0, 8, 0, 3, 0, 0, 1},
-                {7, 0, 0, 0, 2, 0, 0, 0, 6},
-                {0, 6, 0, 0, 0, 0, 2, 8, 0},
-                {0, 0, 0, 4, 1, 9, 0, 0, 5},
-                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
         };
 
-        show(board, 0, 4);
-
-
+        SudokuSolver solver = new SudokuSolver();
+        if (solver.solveSudoku(board)) {
+            solver.printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
     }
+
+
 }
--- Java/Klausur2024/src/SudokuSolver.java
+++ Java/Klausur2024/src/SudokuSolver.java
@@ -7,17 +7,53 @@ public class SudokuSolver {
 
     private boolean solve(int[][] board, int row, int col) {
         if (row ==9)return true;
-        if (col ==9) return solve(board,row+1,col);
+        if (col ==9) return solve(board,row+1,0);
         if (board[row][col]!=0) return solve(board,row,col+1);
 
-        for (int i=0; i<=9; i++) {
+        for (int i=1; i<=9; i++) {
+            if (isSafe(board,row,col,i)) {
+                board[row][col]=i;
+                if(solve(board,row,col+1)) return true;
+                board[row][col]=0;
 
+            }
 
 
 
 
         }
-return true;
+        return false;
     }
+
+    private boolean isSafe(int[][] board, int row, int col, int num) {
+
+
+        for( int i=0; i<9; i++) {
+            if (board[row][i]==num) return false;
+
+        }
+        for (int i=0; i<9; i++) {
+            if (board[i][col]==num) return false;
+        }
+        int startRow = row -row%3;
+        int startCol = col -col%3;
+
+        for (int i=startRow; i<startRow+3; i++) {
+            for (int j=startCol; j<startCol+3; j++) {
+                if (board[i][j]==num) return false;
+            }
+        }
+        return true;
+    }
+    public void printBoard(int[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int d = 0; d < 9; d++) {
+                System.out.print(board[r][d]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
 
 }
